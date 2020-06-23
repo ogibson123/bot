e207ea30d161bfa323aca2af3b9bad62c2eba98c
Oscar <ogibson@umass.edu>
2020-06-23 03:30:09 -0400

    Added random meme command

---
+++ commands/meme.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const randomPuppy = require("random-puppy");
+
+module.exports = {
+    name: "meme",
+    run: async(bot, message, args) => {
+        fetchRandomMeme(message);
+    }
+}
+
+async function fetchRandomMeme(message){
+    const subs = ["memes", "me_irl", "deepfriedmemes", "dankmemes"];
+    const chosenSub = subs[Math.floor(Math.random()*subs.length)]
+    const img = await randomPuppy(chosenSub);
+    const embed = new Discord.MessageEmbed()
+    .setColor("#00b524")
+    .setImage(img)
+
+    message.channel.send({embed: embed});
+}
